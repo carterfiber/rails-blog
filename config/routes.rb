a635735c69f470698a5fc5556aa715621639cdcc
carterfiber <gerald@carterfiber.com>
2017-09-20 10:39:55 -0400

    added user page, pagination, and customer urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comments
   resources :posts
-  get  'index' => 'practice#index'
+  get  'index' => 'posts#index'
 
   post 'index' => 'practice#index'
 
   get  'about' => 'practice#about'
+
+  get  '/:name' => 'posts#user_posts', as: :user_posts
 
   # root 'practice#index'
   root 'posts#index'
